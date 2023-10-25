8cc5f4a2500de8d8c2b64b68f750cb8f5f991cf2
sikkzz <sling0623@gmail.com>
2023-10-26 04:31:06 +0900

    issue #19 design: header profile, dropdown component styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,12 @@
+import { useState, useRef, useEffect } from "react";
+
 import {
 	HeaderMenuIcon,
-	LogoIcon,
+	HeaderLogoIcon,
 	HeaderMoreIcon,
-	LoginIcon,
+	LoginPersonIcon,
+	HeaderUploadIcon,
+	HeaderNotificationIcon,
 } from "@/constants/Icon/icon";
 
 import {
@@ -12,11 +16,38 @@ import {
 	ButtonDiv,
 	LogoDiv,
 	HeaderRightDiv,
+	RightInnerDiv,
 	LoginDiv,
+	ProfileDiv,
 } from "./Header.styles";
+import ProfileDropdown from "./ProfileDropdown";
 import SearchBar from "./Search/SearchBar";
 
 const Header = () => {
+	const isLogin = true;
+
+	const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+
+	const profileRef = useRef<HTMLButtonElement>(null);
+
+	const handleDropdown = (e: CustomEvent<MouseEvent>) => {
+		const target = e.target as HTMLImageElement;
+
+		if (!profileRef?.current?.contains(target)) {
+			setIsDropdownOpen(!isDropdownOpen);
+		}
+	};
+
+	useEffect(() => {
+		if (isDropdownOpen) {
+			window.addEventListener("click", handleDropdown as EventListener);
+		}
+
+		return () => {
+			window.addEventListener("click", handleDropdown as EventListener);
+		};
+	});
+
 	return (
 		<HeaderLayout>
 			<HeaderInnerDiv>
@@ -27,7 +58,7 @@ const Header = () => {
 						</button>
 					</ButtonDiv>
 					<LogoDiv>
-						<LogoIcon />
+						<HeaderLogoIcon />
 						<h2>YouTube</h2>
 					</LogoDiv>
 				</HeaderLeftDiv>
@@ -35,15 +66,43 @@ const Header = () => {
 				<SearchBar />
 
 				<HeaderRightDiv>
-					<ButtonDiv>
-						<button type="button">
-							<HeaderMoreIcon color="#fff" />
-						</button>
-					</ButtonDiv>
-					<LoginDiv>
-						<LoginIcon />
-						<h2>로그인</h2>
-					</LoginDiv>
+					{isLogin ? (
+						<RightInnerDiv>
+							<ButtonDiv>
+								<button type="button">
+									<HeaderUploadIcon color="#fff" />
+								</button>
+							</ButtonDiv>
+							<ButtonDiv>
+								<button type="button">
+									<HeaderNotificationIcon color="#fff" />
+								</button>
+							</ButtonDiv>
+							<ProfileDiv>
+								<button type="button" ref={profileRef}>
+									<img
+										src="https://yt3.ggpht.com/mut5Yu6ZJVe6YKSKOPkBF6XtWzEpKspOfl3WESzhHa40hy0u8RYPW7S6jy4X5yWCDEtrDxH6Cw=s88-c-k-c0x00ffffff-no-rj"
+										alt="profile"
+										onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+									/>
+								</button>
+
+								{isDropdownOpen && <ProfileDropdown />}
+							</ProfileDiv>
+						</RightInnerDiv>
+					) : (
+						<RightInnerDiv>
+							<ButtonDiv>
+								<button type="button">
+									<HeaderMoreIcon color="#fff" />
+								</button>
+							</ButtonDiv>
+							<LoginDiv>
+								<LoginPersonIcon />
+								<h2>로그인</h2>
+							</LoginDiv>{" "}
+						</RightInnerDiv>
+					)}
 				</HeaderRightDiv>
 			</HeaderInnerDiv>
 		</HeaderLayout>
--- src/constants/Icon/icon.ts
+++ src/constants/Icon/icon.ts
@@ -7,10 +7,11 @@ import {
 	AiOutlineInfoCircle as SideInfoIcon,
 } from "react-icons/ai";
 import {
-	BsPersonCircle as LoginIcon,
+	BsPersonCircle as LoginPersonIcon,
 	BsFillCollectionPlayFill as SideSubIcon,
 	BsArrowCounterclockwise as SideShowIcon,
 	BsFlag as SideReportIcon,
+	BsBell as HeaderNotificationIcon,
 } from "react-icons/bs";
 import { FaGamepad as SideGameIcon } from "react-icons/fa";
 import { FiRadio as SideLiveIcon } from "react-icons/fi";
@@ -25,19 +26,30 @@ import {
 import {
 	RiVideoLine as SideMyVideoIcon,
 	RiMusicLine as SideMusicIcon,
+	RiVideoAddLine as HeaderUploadIcon,
 } from "react-icons/ri";
 
 import HeaderMenuIcon from "@/assets/icon/ic-header-menu.svg?react";
-import LogoIcon from "@/assets/icon/ic-logo.svg?react";
+import HeaderLogoIcon from "@/assets/icon/ic-logo.svg?react";
+import ProfileCenterIcon from "@/assets/icon/ic-profile-center.svg?react";
+import ProfileChangeIcon from "@/assets/icon/ic-profile-change.svg?react";
+import ProfileChannelIcon from "@/assets/icon/ic-profile-channel.svg?react";
+import ProfileLogoutIcon from "@/assets/icon/ic-profile-logout.svg?react";
+import ProfileOpinionIcon from "@/assets/icon/ic-profile-opinion.svg?react";
+import ProfileSettingIcon from "@/assets/icon/ic-profile-setting.svg?react";
+import ProfileStudioIcon from "@/assets/icon/ic-profile-studio.svg?react";
+import ProfileThemeIcon from "@/assets/icon/ic-profile-theme.svg?react";
 import SearchIcon from "@/assets/icon/ic-search.svg?react";
 import ShortsIcon from "@/assets/icon/ic-shorts.svg?react";
 
 export {
 	HeaderMenuIcon,
-	LogoIcon,
+	HeaderUploadIcon,
+	HeaderNotificationIcon,
+	HeaderLogoIcon,
 	SearchIcon,
 	HeaderMoreIcon,
-	LoginIcon,
+	LoginPersonIcon,
 	SideHomeIcon,
 	SideShortsIcon,
 	SideSubIcon,
@@ -57,4 +69,12 @@ export {
 	SideCenterIcon,
 	SideInfoIcon,
 	ShortsIcon,
+	ProfileChannelIcon,
+	ProfileChangeIcon,
+	ProfileLogoutIcon,
+	ProfileStudioIcon,
+	ProfileCenterIcon,
+	ProfileOpinionIcon,
+	ProfileSettingIcon,
+	ProfileThemeIcon,
 };
