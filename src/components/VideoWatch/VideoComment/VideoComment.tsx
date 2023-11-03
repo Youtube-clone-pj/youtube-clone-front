3035efba419d6bc06a3b074fe43fbf14b9982aa2
sikkzz <sling0623@gmail.com>
2023-11-04 02:48:23 +0900

    issue #22 design: comment input styling

--- src/components/VideoWatch/VideoComment/VideoComment.tsx
+++ src/components/VideoWatch/VideoComment/VideoComment.tsx
@@ -9,12 +9,25 @@ import {
 	TitleDiv,
 	SortDiv,
 	BottomDiv,
+	BottomInnerDiv,
+	ProfileDiv,
+	CommentDiv,
+	CommentInputDiv,
+	CommentButtonDiv,
+	ButtonInnerDiv,
 } from "./VideoComment.styles";
 
 const VideoComment = () => {
+	const dropdownRef = useRef<HTMLDivElement>(null);
+
 	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-	const dropdownRef = useRef<HTMLDivElement>(null);
+	const [isInputClick, setIsInputClick] = useState(false);
+	const [inputValue, setInputValue] = useState("");
+
+	const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInputValue(e.target.value);
+	};
 
 	const handleDropdown = (e: CustomEvent<MouseEvent>) => {
 		const target = e.target as HTMLDivElement;
@@ -53,7 +66,46 @@ const VideoComment = () => {
 					</SortDiv>
 				</TitleDiv>
 			</TopDiv>
-			<BottomDiv></BottomDiv>
+
+			<BottomDiv>
+				<BottomInnerDiv>
+					<ProfileDiv>
+						<a href="#">
+							<img
+								src="https://yt3.ggpht.com/JvXwi2zyrNnKLdVjp5MIYEakcH5RHWKDIajrPAcCHYjeWFsgNS3az_eX8BT8a8YhjbXiDX-UJQ=s68-c-k-c0x00ffffff-no-rj"
+								alt="profile"
+							/>
+						</a>
+					</ProfileDiv>
+					<CommentDiv>
+						<CommentInputDiv>
+							<form>
+								<input
+									type="text"
+									placeholder="댓글 추가..."
+									onClick={() => setIsInputClick(true)}
+									onChange={handleInputValue}
+								/>
+							</form>
+						</CommentInputDiv>
+						<CommentButtonDiv $isclick={isInputClick}>
+							<ButtonInnerDiv>
+								<button
+									className="cancel"
+									onClick={() => setIsInputClick(false)}
+								>
+									취소
+								</button>
+								<button
+									className={inputValue === "" ? "comment" : "comment active"}
+								>
+									댓글
+								</button>
+							</ButtonInnerDiv>
+						</CommentButtonDiv>
+					</CommentDiv>
+				</BottomInnerDiv>
+			</BottomDiv>
 		</VideoCommentLayout>
 	);
 };
