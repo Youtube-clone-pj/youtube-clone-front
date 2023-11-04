298b5ebdfb9f763736b72ca28f3692bc1245d11c
sikkzz <sling0623@gmail.com>
2023-11-04 22:55:23 +0900

    issue #22 design: comment header, text styling

--- src/components/VideoWatch/VideoComment/VideoCommentContent/VideoCommentContent.styles.ts
+++ src/components/VideoWatch/VideoComment/VideoCommentContent/VideoCommentContent.styles.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
-export const VideoCommentContentLayout = styled.div``;
+export const VideoCommentContentLayout = styled.div`
+	margin-top: 30px;
+`;
 
 export const VideoCommentDiv = styled.div`
 	margin-bottom: 16px;
@@ -8,8 +10,94 @@ export const VideoCommentDiv = styled.div`
 
 export const CommentDiv = styled.div`
 	display: flex;
+	gap: 16px;
 `;
 
-export const ProfileDiv = styled.div``;
+export const ProfileDiv = styled.div`
+	width: 40px;
+	height: 40px;
+
+	& > a > img {
+		width: 100%;
+		height: 100%;
+		border-radius: 50%;
+	}
+`;
+
+export const ContentDiv = styled.div`
+	display: flex;
+	flex: 1 1;
+	flex-basis: 1e-9px;
+	flex-direction: column;
+	gap: 4px;
+`;
+
+export const HeaderDiv = styled.div`
+	display: flex;
+
+	& > h3 > a {
+		color: ${({ theme: { colors } }) => colors.white_primary};
+		display: block;
+		font-size: 0.95rem;
+		font-weight: 500;
+		line-height: 1.4rem;
+		margin-bottom: 2px;
+		margin-right: 4px;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+
+	& > h4 {
+		color: ${({ theme: { colors } }) => colors.gray};
+		font-family: Roboto, Arial, sans-serif;
+		font-size: 0.8rem;
+		line-height: 1.4rem;
+		white-space: nowrap;
+	}
+`;
+
+export const CommentTextDiv = styled.div`
+	width: 100%;
+	overflow: auto;
+
+	& > button {
+		color: ${({ theme: { colors } }) => colors.gray};
+		font-family: Roboto, Arial, sans-serif;
+		font-size: 0.9rem;
+		font-weight: 500;
+		line-height: 1.5rem;
+		text-transform: none;
+		margin-top: 4px;
+		display: inline-block;
+		cursor: pointer;
+	}
+
+	& > .disabled {
+		display: none;
+	}
+`;
+
+export const TextDiv = styled.div<{ $ismore: boolean }>`
+	display: ${({ $ismore }) => ($ismore ? "block" : "-webkit-box")};
+	-webkit-box-orient: vertical;
+	-webkit-line-clamp: 4;
+	color: ${({ theme: { colors } }) => colors.white_primary};
+	max-height: none;
+	overflow: hidden;
+	font-family: Roboto, Arial, sans-serif;
+	font-size: 0.9rem;
+	line-height: 1.5rem;
+	overflow-wrap: anywhere;
+	white-space: pre-wrap;
+
+	& > span > a {
+		color: ${({ theme: { colors } }) => colors.blue_primary};
+	}
+`;
+
+export const ContentButtonDiv = styled.div``;
+
+export const OptionButtonDiv = styled.div``;
 
 export const ReplyDiv = styled.div``;
--- src/components/VideoWatch/VideoComment/VideoCommentContent/VideoCommentContent.tsx
+++ src/components/VideoWatch/VideoComment/VideoCommentContent/VideoCommentContent.tsx
@@ -1,7 +1,109 @@
-import { VideoCommentContentLayout } from "./VideoCommentContent.styles";
+import { useState } from "react";
+
+import {
+	VideoCommentContentLayout,
+	VideoCommentDiv,
+	CommentDiv,
+	ProfileDiv,
+	ContentDiv,
+	HeaderDiv,
+	CommentTextDiv,
+	TextDiv,
+	ContentButtonDiv,
+	OptionButtonDiv,
+	ReplyDiv,
+} from "./VideoCommentContent.styles";
 
 const VideoCommentContent = () => {
-	return <VideoCommentContentLayout />;
+	const [isMore, setIsMore] = useState(false);
+
+	const handleCommentMore = () => {
+		setIsMore(!isMore);
+	};
+
+	return (
+		<VideoCommentContentLayout>
+			<VideoCommentDiv>
+				<CommentDiv>
+					<ProfileDiv>
+						<a href="#">
+							<img
+								src="https://yt3.ggpht.com/JvXwi2zyrNnKLdVjp5MIYEakcH5RHWKDIajrPAcCHYjeWFsgNS3az_eX8BT8a8YhjbXiDX-UJQ=s68-c-k-c0x00ffffff-no-rj"
+								alt="profile"
+							/>
+						</a>
+					</ProfileDiv>
+					<ContentDiv>
+						<HeaderDiv>
+							<h3>
+								<a href="#">@user-김준식</a>
+							</h3>
+							<h4>10개월 전(수정됨)</h4>
+						</HeaderDiv>
+						<CommentTextDiv>
+							<TextDiv $ismore={isMore}>
+								<span>
+									이번 플리는 FLO (플로) 해외 팝 TOP 100곡 기준으로
+									제작되었습니다:)
+								</span>
+								<br />
+								<span>
+									FLO에서 [둘러보기] → [상황]에서 제 플리들을 만나보실 수 있어요
+								</span>
+								<br />
+								<span>
+									💙 광고제거 👉{" "}
+									<a id="tag" href="#!">
+										05:31:04
+									</a>{" "}
+									클릭 후 🔄 다시보기
+								</span>
+								<br />
+								<span>
+									[
+									<a id="tag" href="#!">
+										00:01
+									</a>
+									] 1위 The Kid LAROI & Justin Bieber - STAY{" "}
+								</span>
+								<br />
+								<span>
+									[
+									<a id="tag" href="#!">
+										02:18
+									</a>
+									] 2위 GAYLE - abcdefu{" "}
+								</span>
+								<br />
+								<span>
+									[
+									<a id="tag" href="#!">
+										05:05
+									</a>
+									] 3위 GAYLE - abcdefu{" "}
+								</span>
+							</TextDiv>
+							<button
+								className={isMore ? "" : "disabled"}
+								onClick={handleCommentMore}
+							>
+								간략히
+							</button>
+							<button
+								className={isMore ? "disabled" : ""}
+								onClick={handleCommentMore}
+							>
+								자세히 보기
+							</button>
+						</CommentTextDiv>
+						<ContentButtonDiv></ContentButtonDiv>
+					</ContentDiv>
+					<OptionButtonDiv></OptionButtonDiv>
+				</CommentDiv>
+				<ReplyDiv></ReplyDiv>
+			</VideoCommentDiv>
+		</VideoCommentContentLayout>
+	);
 };
 
 export default VideoCommentContent;
